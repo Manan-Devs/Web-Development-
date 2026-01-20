3a50b9a9cc4cd707277fdee6743b0e43c3fa8422
Manan Bhatt <mananbhatt1010@gmail.com>
2026-01-20 21:19:24 +0530

    Updated

--- Frontend/React/Projects/ReactForm/src/Form.jsx
+++ Frontend/React/Projects/ReactForm/src/Form.jsx
@@ -1,121 +1,64 @@
 import {useForm} from "react-hook-form";
 import "./Form.css";
-// import { zodResolver } from '@hookform/resolvers/zod';
-// import {email,z} from zod;
+import { zodResolver } from '@hookform/resolvers/zod';
+import {email, z} from 'zod'
 
-// const formSchema = z.object({
-//     userName:z.string().min(3, "minimum length should be 3").max(20, "maximum length should be 20"),
+const formSchema = z.object({
+    name:z.string().min(3, "minimum length should be 3").max(20, "maximum length should be 20"),
     
-// });
+});
 
 
  function Form () {
     
-  const { register,handleSubmit, formState: {errors, }} = useForm();
+  const {register, handleSubmit, formState: { errors }} = useForm({
+          resolver: zodResolver(formSchema)
+      });
    
-     function submitForm(data) {
-           console.log(data);
-           
-     }
+     function submitForm(data){
+       console.log(data);
+    }
 
-  return ( 
+      console.log("Render");
+      
 
-      <> 
-         <form onSubmit={handleSubmit(submitForm)}>
-            <div className="item">
-              <label htmlFor="first">Username: </label>
-              <input id="first" {...register ('name', 
-                 {required:"Name can't be empty"}
-              )} />
-                {errors.name && <span>{errors.name.message}</span>}
-           </div> 
-
-           <div className="item" >
-              <label htmlFor="first">Email: </label>
-              <input id="first" {...register ('email', 
-                
-              )} />
-                {errors.name && <span>{errors.name.message}</span>}
-           </div> 
-
-           <div className="item">
-              <label htmlFor="first">PhoneNumber: </label>
-              <input id="first" {...register ('name', 
-                 {required:"Name can't be empty"}
-              )} />
-                {errors.name && <span>{errors.name.message}</span>}
-           </div> 
-
-           <div className="item">
-              <label htmlFor="first">Password: </label>
-              <input id="first" {...register ('name', 
-                 {required:"Name can't be empty"}
-              )} />
-                {errors.name && <span>{errors.name.message}</span>}
-           </div> 
-
-           <div className="item">
-              <label htmlFor="first">Age: </label>
-              <input id="first" {...register ('age', 
-                 {required:"Name can't be empty"}
-              )} />
-                {errors.name && <span>{errors.name.message}</span>}
-           </div> 
-           <div className="item">
-              <label htmlFor="first">Country: </label>
-              <input id="first" {...register ('name', 
-                 {required:"Name can't be empty"}
-              )} />
-                {errors.name && <span>{errors.name.message}</span>}
-           </div> 
-           <div className="item">
-              <label htmlFor="first">State: </label>
-              <input id="first" {...register ('name', 
-                 {required:"Name can't be empty"}
-              )} />
-                {errors.name && <span>{errors.name.message}</span>}
-           </div> 
-           <div className="item">
-              <label htmlFor="first">City: </label>
-              <input id="first" {...register ('name', 
-                 {required:"Name can't be empty"}
-              )} />
-                {errors.name && <span>{errors.name.message}</span>}
-           </div> 
-           <div className="item">
-              <label htmlFor="first">: </label>
-              <input id="first" {...register ('name', 
-                 {required:"Name can't be empty"}
-              )} />
-                {errors.name && <span>{errors.name.message}</span>}
-           </div> 
-           <div className="item">
-              <label htmlFor="first">Name: </label>
-              <input id="first" {...register ('name', 
-                 {required:"Name can't be empty"}
-              )} />
-                {errors.name && <span>{errors.name.message}</span>}
-           </div> 
-           <div className="item">
-              <label htmlFor="first">Name: </label>
-              <input id="first" {...register ('name', 
-                 {required:"Name can't be empty"}
-              )} />
-                {errors.name && <span>{errors.name.message}</span>}
-           </div> 
-           <div className="item">
-              <label htmlFor="first">Name: </label>
-              <input id="first" {...register ('name', 
-                 {required:"Name can't be empty"}
-              )} />
-                {errors.name && <span>{errors.name.message}</span>}
-           </div> 
-         </form>
-            <div className="submit">
+  return(
+        <>
+        <form onSubmit={handleSubmit(submitForm)}>
+         <div className="item">
+            <label htmlFor="first">Name: </label>
+            <input id="first" {...register('name')} />
+            {errors.name && <span>{errors.name.message}</span>}
+         </div>
+          <div className="item">
+            <label htmlFor="fourth">Email: </label>
+            <input id="fourth" {...register('email')} />
+            {errors.email && <span>{errors.email.message}</span>}
+         </div>
+          <div>
+            <label htmlFor="second">Age: </label>
+            <input id="second" {...register('age')} />
+            {errors.age && <span>{errors.age.message}</span>}
+         </div>
+         <div className="item">
+            <label htmlFor="third">Password: </label>
+            <input type="password" id="third" {...register('password'
+            )} />
+            {errors.password && <span>{errors.password.message}</span>}
+         </div>
+         <div className="item">
+            <label htmlFor="fifth">Confirm Password: </label>
+            <input type="password" id="fifth" {...register('confirm'
+            )} />
+            {errors.confirm && <span>{errors.confirm.message}</span>}
+         </div>
+          <div className="submit">
              <button>Submit</button>
-           </div>
+          </div>
+         </form>
+          
          
-       </>
+        </>
   )
    
 }
