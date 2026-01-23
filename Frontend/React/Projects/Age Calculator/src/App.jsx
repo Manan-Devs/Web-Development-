60b6ec818de39bfa22aace937a2cffdc3464720f
Manan Bhatt <mananbhatt1010@gmail.com>
2026-01-23 22:30:24 +0530

    Updated

--- Frontend/React/Projects/Age Calculator/src/App.jsx
+++ Frontend/React/Projects/Age Calculator/src/App.jsx
@@ -1,30 +1,44 @@
-import "./App.css"
+import "./App.css";
+import { useState} from "react";
+
+
+
+
 
 function App() {
+
+   const [years,months,days] = useState();
+    
+
+   
   return (
     <div className="ageCalculatorContainer">
-        <div className="data">
+      <div className="data">
            <div className="inputs">
               <div className="day">
-                 <h2>Day</h2>
-                 <input type="number" />
+                 <h3>Day</h3>
+                 <input value={days} placeholder="DD" type="number" />
               </div>
              <div className="month">
-                <h2>Month</h2>
-                <input type="number" />
+                <h3>Month</h3>
+                <input value={months} placeholder="MM" type="number" />
               </div>
              <div className="year">
-                <h2>Year</h2>
-                <input type="number" />
+                <h3>Year</h3>
+                <input value = {years}placeholder="YYY" type="number" />
               </div>
            </div>
            <div className="space">
-             
+              <hr />
+             <img src="C:\Users\manan\OneDrive\Desktop\Web Development\Frontend\React\Projects\Age Calculator\src\assets\images\icon-arrow.svg" alt="" />
            </div>
+           
         </div>
 
         <div className="age">
-            
+            <h1><span>--</span>&nbsp;Years</h1>
+            <h1><span>--</span>&nbsp;Months</h1>
+            <h1><span>--</span>&nbsp;Days</h1>
         </div>
     </div>
   )
